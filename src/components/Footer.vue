ceda21404aab74100fc30f72ec8585e6dec3f16c
kaloyankolev13 <63497199+kaloyankolev13@users.noreply.github.com>
2021-12-14 16:57:02 +0100

    finished the homepage

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,208 @@
 <template>
   <div id="container">
-    <h1>FOOTER</h1>
+    <div class="footer">
+      <div class="newsletter">
+        <p>Subscribe to our newsletteer</p>
+        <p class="btn">Register</p>
+      </div>
+      <div class="social">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+          width="64"
+          height="65"
+          viewBox="0 0 64 65"
+        >
+          <defs>
+            <clipPath id="clip-path">
+              <rect
+                id="Rectangle_21"
+                data-name="Rectangle 21"
+                width="12.757"
+                height="24"
+                fill="#fff"
+              />
+            </clipPath>
+          </defs>
+          <g
+            id="Group_19"
+            data-name="Group 19"
+            transform="translate(-1361 -3561)"
+          >
+            <ellipse
+              id="Ellipse_6"
+              data-name="Ellipse 6"
+              cx="32"
+              cy="32.5"
+              rx="32"
+              ry="32.5"
+              transform="translate(1361 3561)"
+              fill="#e8af30"
+            />
+            <g
+              id="Group_15"
+              data-name="Group 15"
+              transform="translate(1386.424 3581.5)"
+            >
+              <g id="Group_10" data-name="Group 10" clip-path="url(#clip-path)">
+                <path
+                  id="Path_9"
+                  data-name="Path 9"
+                  d="M11.921,13.4l.661-4.311H8.446V6.29a2.156,2.156,0,0,1,2.43-2.329h1.881V.291A22.959,22.959,0,0,0,9.419,0C6.012,0,3.786,2.064,3.786,5.8V9.087H0V13.4H3.786V23.819a15.087,15.087,0,0,0,4.66,0V13.4Z"
+                  fill="#fff"
+                />
+              </g>
+            </g>
+          </g>
+        </svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+          width="64"
+          height="65"
+          viewBox="0 0 64 65"
+        >
+          <defs>
+            <clipPath id="clip-path">
+              <rect
+                id="Rectangle_18"
+                data-name="Rectangle 18"
+                width="25.803"
+                height="25.803"
+                fill="#fff"
+              />
+            </clipPath>
+          </defs>
+          <g
+            id="Group_20"
+            data-name="Group 20"
+            transform="translate(-1446 -3561)"
+          >
+            <ellipse
+              id="Ellipse_7"
+              data-name="Ellipse 7"
+              cx="32"
+              cy="32.5"
+              rx="32"
+              ry="32.5"
+              transform="translate(1446 3561)"
+              fill="#e8af30"
+            />
+            <g
+              id="Group_5"
+              data-name="Group 5"
+              transform="translate(1470.099 3588.599)"
+            >
+              <g
+                id="Group_4"
+                data-name="Group 4"
+                transform="translate(-5 -7)"
+                clip-path="url(#clip-path)"
+              >
+                <path
+                  id="Path_4"
+                  data-name="Path 4"
+                  d="M12.9,0C9.4,0,8.958.015,7.582.078a9.425,9.425,0,0,0-3.132.6A6.3,6.3,0,0,0,2.166,2.166,6.3,6.3,0,0,0,.678,4.45a9.425,9.425,0,0,0-.6,3.132C.015,8.958,0,9.4,0,12.9s.015,3.943.078,5.319a9.41,9.41,0,0,0,.6,3.131,6.292,6.292,0,0,0,1.488,2.285A6.315,6.315,0,0,0,4.45,25.125a9.457,9.457,0,0,0,3.132.6c1.376.063,1.816.078,5.319.078s3.943-.015,5.319-.078a9.457,9.457,0,0,0,3.132-.6,6.6,6.6,0,0,0,3.772-3.773,9.442,9.442,0,0,0,.6-3.131c.063-1.376.078-1.816.078-5.319s-.015-3.943-.078-5.319a9.457,9.457,0,0,0-.6-3.132,6.3,6.3,0,0,0-1.488-2.284A6.3,6.3,0,0,0,21.353.678a9.425,9.425,0,0,0-3.132-.6C16.845.015,16.405,0,12.9,0m0,2.325c3.445,0,3.853.013,5.213.076a7.133,7.133,0,0,1,2.4.443,4.016,4.016,0,0,1,1.484.965,4.015,4.015,0,0,1,.965,1.484,7.126,7.126,0,0,1,.443,2.4c.062,1.361.076,1.769.076,5.213s-.014,3.853-.076,5.213a7.133,7.133,0,0,1-.443,2.4,4.269,4.269,0,0,1-2.449,2.449,7.133,7.133,0,0,1-2.4.443c-1.361.062-1.769.076-5.213.076s-3.853-.014-5.213-.076a7.133,7.133,0,0,1-2.4-.443,4.015,4.015,0,0,1-1.484-.965,4.016,4.016,0,0,1-.965-1.484,7.133,7.133,0,0,1-.443-2.4c-.063-1.361-.076-1.769-.076-5.213s.013-3.853.076-5.213a7.126,7.126,0,0,1,.443-2.4,4.016,4.016,0,0,1,.965-1.484,4.016,4.016,0,0,1,1.484-.965,7.133,7.133,0,0,1,2.4-.443c1.361-.063,1.769-.076,5.213-.076"
+                  transform="translate(0 0)"
+                  fill="#fff"
+                />
+                <path
+                  id="Path_5"
+                  data-name="Path 5"
+                  d="M11.607,15.908a4.3,4.3,0,1,1,4.3-4.3,4.3,4.3,0,0,1-4.3,4.3m0-10.926a6.625,6.625,0,1,0,6.625,6.625,6.624,6.624,0,0,0-6.625-6.625"
+                  transform="translate(1.295 1.295)"
+                  fill="#fff"
+                />
+                <path
+                  id="Path_6"
+                  data-name="Path 6"
+                  d="M17.574,5.093a1.548,1.548,0,1,1-1.548-1.548,1.548,1.548,0,0,1,1.548,1.548"
+                  transform="translate(3.762 0.921)"
+                  fill="#fff"
+                />
+              </g>
+            </g>
+          </g>
+        </svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+          width="64"
+          height="65"
+          viewBox="0 0 64 65"
+        >
+          <defs>
+            <clipPath id="clip-path">
+              <rect
+                id="Rectangle_19"
+                data-name="Rectangle 19"
+                width="24"
+                height="16.8"
+                transform="translate(0 0)"
+                fill="#fff"
+              />
+            </clipPath>
+          </defs>
+          <g
+            id="Group_21"
+            data-name="Group 21"
+            transform="translate(-1529 -3561)"
+          >
+            <ellipse
+              id="Ellipse_8"
+              data-name="Ellipse 8"
+              cx="32"
+              cy="32.5"
+              rx="32"
+              ry="32.5"
+              transform="translate(1529 3561)"
+              fill="#e8af30"
+            />
+            <g
+              id="Group_7"
+              data-name="Group 7"
+              transform="translate(1549 3585.151)"
+            >
+              <g id="Group_6" data-name="Group 6" clip-path="url(#clip-path)">
+                <path
+                  id="Path_7"
+                  data-name="Path 7"
+                  d="M23.5,2.623A3.007,3.007,0,0,0,21.377.5C19.5,0,12,0,12,0S4.495,0,2.623.5A3.007,3.007,0,0,0,.5,2.623,31.207,31.207,0,0,0,0,8.4a31.193,31.193,0,0,0,.5,5.776A3.005,3.005,0,0,0,2.623,16.3c1.872.5,9.377.5,9.377.5s7.5,0,9.377-.5A3,3,0,0,0,23.5,14.176,31.254,31.254,0,0,0,24,8.4a31.268,31.268,0,0,0-.5-5.777M9.6,12V4.8l6.235,3.6L9.6,12"
+                  fill="#fff"
+                />
+              </g>
+            </g>
+          </g>
+        </svg>
+      </div>
+    </div>
+    <div class="footer-text">
+      <div class="col col-1">
+        <p><b>Support us</b></p>
+      </div>
+      <div class="col col-2">
+        <p><b>Visit</b></p>
+        <br />
+        <br />
+        <br />
+        <p>Plan a Visit</p>
+        <p>Ticket Prices</p>
+        <p>Map</p>
+        <p>Student Discounts</p>
+        <p>Host an Event</p>
+        <p>Trapholt Cafe</p>
+      </div>
+      <div class="col col-3">
+        <p><b>Explore</b></p>
+        <br />
+        <br />
+        <br />
+        <p><b>Trapholt Cafe</b></p>
+      </div>
+      <div class="col col-4">
+        <p><b>Location + Hours</b></p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,6 +212,50 @@ export default {};
 
 <style lang="scss" scoped>
 #container {
+  height: 570px;
   background: linear-gradient(35deg, #1a356c, #0f2043);
+  justify-content: space-between;
+}
+.footer {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: auto;
+  padding: 50px 0;
+  width: 72%;
+  border-bottom: 1px solid #fff;
+
+  .newsletter {
+    display: flex;
+    align-items: center;
+    background: #e8af30;
+
+    padding: 20px;
+    .btn {
+      background: #fff;
+      padding: 10px 20px;
+      color: #e8af30;
+      margin-left: 15px;
+    }
+  }
+  .social {
+    display: flex;
+    svg {
+      margin-right: 15px;
+    }
+  }
+}
+.footer-text {
+  display: flex;
+  justify-content: center;
+  margin: auto;
+  width: 100%;
+  .col {
+    padding: 50px;
+    text-align: left;
+  }
+  .col-2 p {
+    margin-bottom: 10px;
+  }
 }
 </style>
