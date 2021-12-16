67703a548ef7deb7aa677617802979e409369a8a
kaloyankolev13 <63497199+kaloyankolev13@users.noreply.github.com>
2021-12-16 15:17:25 +0100

    started improting members code

---
+++ src/views/Exhibition.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <p style="color: #333">{{ exhibitions[1].name }}</p>
+    <BabylonExamples />
+  </div>
+</template>
+
+<script>
+import BabylonExamples from "../components/BabylonExamples.vue";
+export default {
+  components: {
+    BabylonExamples,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/views/ExhibitionsPage.vue
+++ src/views/ExhibitionsPage.vue
@@ -1,17 +1,61 @@
 <template>
-  <div class="about">
-    <BabylonExamples />
+  <div id="container">
+    <ul class="exhibitions">
+      <li
+        v-for="exhibition in exhibitions"
+        :key="exhibition.id"
+        class="featured-exhibitions"
+      >
+        <h1>{{ exhibition.name }}</h1>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import BabylonExamples from "../components/BabylonExamples.vue";
 export default {
-  components: {
-    BabylonExamples,
+  name: "ExhibitionsPage",
+  data() {
+    return {
+      exhibitions: [
+        {
+          id: 1,
+          name: "Exhibition 1",
+          description: "This is the first exhibition",
+        },
+        {
+          id: 2,
+          name: "Exhibition 2",
+          description: "This is the second exhibition",
+        },
+        {
+          id: 3,
+          name: "Exhibition 3",
+          description: "This is the third exhibition",
+        },
+        {
+          id: 4,
+          name: "Exhibition 4",
+          description: "This is the fourth exhibition",
+        },
+        {
+          id: 5,
+          name: "Exhibition 5",
+          description: "This is the fifth exhibition",
+        },
+        {
+          id: 6,
+          name: "Exhibition 6",
+          description: "This is the sixth exhibition",
+        },
+      ],
+    };
   },
 };
 </script>
 
 <style lang="scss" scoped>
+#container {
+  color: red;
+}
 </style>
