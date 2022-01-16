e427caa725f50cc9ac89d5fe6abe2a362c6fbb00
kaloyankolev13 <63497199+kaloyankolev13@users.noreply.github.com>
2022-01-17 00:59:49 +0100

    sub page done

--- src/views/Subscriptions.vue
+++ src/views/Subscriptions.vue
@@ -5,9 +5,14 @@
       <ul>
         <li>> Become a member</li>
         <li>> Give a membership</li>
-        <li>Member News</li>
-        <li>FAQ</li>
+        <li>> Member News</li>
+        <li>> FAQ</li>
       </ul>
+    </div>
+    <div class="subs">
+      <h1>OUR PRICING PLANS</h1>
+      <img src="../assets/images/Subs.png" alt="" />
+      <img src="../assets/images/Subs-0.png" alt="" />
     </div>
   </div>
 </template>
@@ -19,11 +24,28 @@ export default {};
 <style lang="scss" scoped>
 #container {
   background: linear-gradient(35deg, #1a356c, #0f2043);
-  height: 1000px;
-  .link {
+
+  .links {
     width: 72%;
     margin: auto;
     text-align: left;
   }
+}
+
+h1 {
+  padding: 40px;
+}
+
+ul {
+  list-style: none;
+  li {
+    color: #e8af30;
+    margin: 10px;
+  }
+}
+
+img {
+  margin-top: -150px;
+  scale: 0.8;
 }
 </style>
